1c0636151d35137983079d778fc385f97b7db8a4
Coinibal <69815815+Coinibal@users.noreply.github.com>
2021-01-24 12:54:06 -0500

    Update and rename Token.sol to Bitunits.sol

--- Bitunits.sol
+++ Bitunits.sol
@@ -16,7 +16,7 @@ contract Token is ERC20, ERC20Detailed {
     /**
      * @dev Constructor that gives msg.sender all of existing tokens.
      */
-    constructor () public ERC20Detailed("BitUnits", "UNITS", 18) {
-        _mint(msg.sender, 10000000 * (10 ** uint256(decimals())));
+    constructor () public ERC20Detailed("BitUnits Titan", "TITAN", 18) {
+        _mint(msg.sender, 10000 * (10 ** uint256(decimals())));
     }
 }
